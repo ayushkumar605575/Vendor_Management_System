87d49cdcde4315afb3de5265027646a11c07e44a
ayush <ayushkumar605575@gmail.com>
2024-04-30 22:57:48 +0530

    App integration

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'vendors.apps.VendorsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
