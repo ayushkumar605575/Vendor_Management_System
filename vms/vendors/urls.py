7f72accd1c083d07f5de05989f8bda3def17a888
ayush <ayushkumar605575@gmail.com>
2024-05-02 17:37:48 +0530

    urls

--- vms/vendors/urls.py
+++ vms/vendors/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('vendors/', vendorListCreateView, name='vendor-list-create'),
-    path('vendors/<int:pk>/', vendorRetrieveUpdateDeleteView, name='vendor-retrieve-update-delete'),
+    path('vendors/<slug:pk>/', vendorRetrieveUpdateDeleteView, name='vendor-retrieve-update-delete'),
+    path('vendors/<slug:pk>/performance/', vendorPerformanceView, name='vendor-performance'),
     path('purchase_orders/', purchaseOrderListCreateView, name='purchase-order-list-create'),
-    path('purchase_orders/<int:pk>/', purchaseOrderRetrieveUpdateDeleteView, name='purchase-order-retrieve-update-delete'),
-    path('vendors/<int:pk>/performance/', vendorPerformanceView, name='vendor-performance'),
-    path('purchase_orders/<int:pk>/acknowledge/', acknowledgePurchaseOrderView, name='acknowledge-purchase-order'),
+    path('purchase_orders/<slug:pk>/', purchaseOrderRetrieveUpdateDeleteView, name='purchase-order-retrieve-update-delete'),
+    path('purchase_orders/<slug:pk>/acknowledge/', acknowledgePurchaseOrderView, name='acknowledge-purchase-order'),
 ]
--- vms/vms/urls.py
+++ vms/vms/urls.py
@@ -24,8 +24,3 @@ urlpatterns = [
     path('auth/',include('rest_framework.urls',namespace='rest_framework')), 
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
 ]
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('api.urls')),
-#     path('vendors/', include('vendors.urls')),
-# ]
