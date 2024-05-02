45e42bd0d1f2814e3b83304398089b4747764521
ayush <ayushkumar605575@gmail.com>
2024-05-02 17:37:13 +0530

    testing

--- request_testing.py
+++ request_testing.py
@@ -1,13 +1,34 @@
-import requests
+import requests, json
 
+
+cred = {'username':"abhishek", 'password':'abhi@123'}
 
 with requests.Session() as session:
-    response = session.post('http://localhost:8000/api-token-auth/', data={'username':"abhishek", 'password':'abhi@123'})
+    response = session.post('http://localhost:8000/api-token-auth/', data= cred)
+    # print(response.headers)
     
-    print(response.status_code)
-    print(response.json())
+    # print(response.status_code)
+    
+    dat = {'Authorization': f"Token {response.json()['token']}"}
 
-    response = session.post('http://localhost:8000/api/vendors/', data= response.json())
-    print(response.json())
+    print(dat)
+
+    dataa = {
+    "vendor_code": "AIR3543TEL",
+    "name": "Airtel",
+    "contact_details": "Contact 2",
+    "address": "Address 2",
+    "on_time_delivery_rate": 1.5,
+    "quality_rating_avg": 2.4,
+    "average_response_time": 1,
+    "fulfillment_rate": 6.5}
+
+    dataa = {"acknowledgment_date":"2024-05-01T16:55:03Z",}
+
+
+    response = session.patch('http://localhost:8000/api/purchase_orders/SAM3543SUNG0001/acknowledge/', headers = dat, data = dataa)
+    print(response.text)
+    print(response.status_code)
 
     session.close()
+
