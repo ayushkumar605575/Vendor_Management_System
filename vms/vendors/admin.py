9dafbe5656d08d4c09d7b681c801ebb8014b717d
ayush <ayushkumar605575@gmail.com>
2024-05-04 21:52:32 +0530

    admin

--- vms/vendors/admin.py
+++ vms/vendors/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
 # Register your models here.
 admin.site.register(Vendor)
 admin.site.register(PurchaseOrder)
