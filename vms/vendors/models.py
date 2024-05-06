fae49b4ba85f9dc988bdd20021267876a3602337
ayush <ayushkumar605575@gmail.com>
2024-05-06 20:50:16 +0530

    models

--- vms/vendors/models.py
+++ vms/vendors/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.db.models import Avg, F
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db.models import Avg, F
 
 class Vendor(models.Model):
     """
